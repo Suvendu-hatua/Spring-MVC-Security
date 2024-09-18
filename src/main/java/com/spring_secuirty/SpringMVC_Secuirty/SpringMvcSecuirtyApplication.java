1ac88e25ff74165e8c2aa70d08b12022639c3243
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-09-18 22:11:52 +0530

    Adding all the initial files for Spring-MVC-Security Project.

---
+++ src/main/java/com/spring_secuirty/SpringMVC_Secuirty/SpringMvcSecuirtyApplication.java
@@ -0,0 +1,13 @@
+package com.spring_secuirty.SpringMVC_Secuirty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSecuirtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcSecuirtyApplication.class, args);
+	}
+
+}
