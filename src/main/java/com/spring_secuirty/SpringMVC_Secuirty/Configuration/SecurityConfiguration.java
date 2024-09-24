795adf9db49bf230477fabfcd704342298a1e442
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-09-24 22:07:28 +0530

    Adding JPA and mysql dependency and modifying SecurityConfiguration.java

--- src/main/java/com/spring_secuirty/SpringMVC_Secuirty/Configuration/SecurityConfiguration.java
+++ src/main/java/com/spring_secuirty/SpringMVC_Secuirty/Configuration/SecurityConfiguration.java
@@ -6,34 +6,45 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
 
+    //Adding Database Connection for retrieving users from Database.
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-
-        //Adding all the Users --->in-memory user details strategy.
-        //Here we are storing all the passwords with no encoding algorithms=====>{noop}
-        UserDetails suvendu= User.builder()
-                .username("suvendu")
-                .password("{noop}test@123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails sonali= User.builder()
-                .username("sonali")
-                .password("{noop}test@123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-        UserDetails akash= User.builder()
-                .username("akash")
-                .password("{noop}test@123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(suvendu,sonali,akash);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        //Adding all the Users --->in-memory user details strategy.
+//        //Here we are storing all the passwords with no encoding algorithms=====>{noop}
+//        UserDetails suvendu= User.builder()
+//                .username("suvendu")
+//                .password("{noop}test@123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails sonali= User.builder()
+//                .username("sonali")
+//                .password("{noop}test@123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//        UserDetails akash= User.builder()
+//                .username("akash")
+//                .password("{noop}test@123")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(suvendu,sonali,akash);
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
